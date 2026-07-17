0393543b44d579242e545ffadde6260263bb05b1
Ahmed AbdElHafez, Vodafone <ahmad.yousef.hafiz@gmail.com>
2026-07-18 02:40:41 +0300

    add new files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// @ts-ignore: side-effect import of CSS module without type declarations
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -53,7 +53,7 @@ function Home() {
         </section>
       )}
 
-      <BottomNavigation />
+     {/* <BottomNavigation /> */}
     </main>
   );
 }
